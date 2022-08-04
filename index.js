4fbb75efd78b51e8301a45e900bc28ce98be268c
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2022-08-04 11:13:02 +1000

    final fix of merge conflict

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("Hello World");
 
 function addingNumbers(x,y){
@@ -7,6 +6,4 @@ function addingNumbers(x,y){
 }
 
 addingNumbers(5, 10); 
-=======
-console.log("Hello World");
->>>>>>> c715007deb2b90f2dffe1a3c1407aeba8a1fcf2b
+console.log("I made a merge conflict");
